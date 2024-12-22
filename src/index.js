8b6af681b4c51080d53ab62dd09d86ed187ecc17
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-12-22 19:13:57 +0200

    fix in index.js import file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/uploadDir.js';
-import { initMongoConnection } from './db/initMongoDB.js';
-import { setupServer } from './server.js';
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 
 const bootstrap = async () => {
-  await initMongoConnection();
+  await initMongoDB();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
-  setupServer();
+  startServer();
 };
 
 bootstrap();
